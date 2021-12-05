23d520ecab351d89910cbf2eda49d0968340e02d
Michael Daum <michael.daum@epicgames.com>
2021-12-05 00:14:18 -0500

    Day 3b

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -9,7 +9,7 @@ fn read_lines(file_name: &std::string::String) -> Vec<String> {
     reader.lines().map(|r| r.unwrap()).collect()
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 enum Bit {
     Zero,
     One,
@@ -34,7 +34,6 @@ fn evaluate(bits: &Vec<Bit>) -> i64 {
 fn calculate_majority_bits(vals: &Vec<Vec<Bit>>) -> Vec<Bit> {
     let num_bits = vals[0].len();
     let num_vals = vals.len();
-
     vals.iter()
         .fold(vec![0; num_bits], |a, b| {
             a.into_iter()
@@ -44,13 +43,26 @@ fn calculate_majority_bits(vals: &Vec<Vec<Bit>>) -> Vec<Bit> {
         })
         .into_iter()
         .map(|n| {
-            if (n * 2) as usize <= num_vals {
+            if ((n * 2) as usize) < num_vals {
                 Bit::Zero
             } else {
                 Bit::One
             }
         })
         .collect()
+}
+
+fn calculate_rating(bits: &Vec<Bit>, vals: &Vec<Vec<Bit>>, should_invert:bool) -> i64 {
+    let mut filtered_vals = vals.to_vec();
+	let mut filtered_bits = bits.to_vec();
+	let mut i = 0;
+    while filtered_vals.len() > 1 {
+        filtered_vals = filtered_vals.into_iter().filter(|v| v[i] == filtered_bits[i]).collect();
+		filtered_bits  = calculate_majority_bits(&filtered_vals);
+		if should_invert {filtered_bits = invert(&filtered_bits)};
+		i += 1;
+    }
+    evaluate(&filtered_vals[0])
 }
 
 fn main() {
@@ -68,9 +80,14 @@ fn main() {
         .collect();
 
     let majority_bits = calculate_majority_bits(&vals);
+    let minority_bits = invert(&majority_bits);
 
     println!("majority_bits {:?}", majority_bits);
     let gamma = evaluate(&majority_bits);
-    let epsilon = evaluate(&invert(&majority_bits));
+    let epsilon = evaluate(&minority_bits);
     println!("values {} * {} = {}", gamma, epsilon, gamma * epsilon);
+
+	let oxygen = calculate_rating(&majority_bits, &vals, false);
+	let co2 = calculate_rating(&minority_bits, &vals, true);
+	println!("ratings {} * {} = {}", oxygen, co2, oxygen * co2);
 }
