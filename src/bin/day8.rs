a0d2888ea0d7dda0c078a08bef5aa0ecb853beb7
Michael Daum <michael.daum@epicgames.net>
2021-12-11 17:51:35 -0500

    Refactor towards Day 8b

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -13,15 +13,28 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let data_file_path = &args[1];
     let lines = read_lines(data_file_path);
-    let uniques = lines
+    let tokens: Vec<Vec<Vec<_>>> = lines
         .iter()
-        .map(|l| l.split('|').nth(1).unwrap().split_whitespace());
-    let n: usize = uniques
-        .map(|v| {
-            v.map(|s| s.len())
+        .map(|l| {
+            l.split('|')
+                .map(|s| s.split_whitespace().collect())
+                .collect()
+        })
+        .collect();
+
+    let display_tuples: Vec<(&Vec<_>, &Vec<_>)> = tokens
+        .iter()
+        .map(|v| (v.iter().next().unwrap(), v.iter().nth(1).unwrap()))
+        .collect();
+    let num_easy_identified: usize = display_tuples
+        .iter()
+        .map(|(_, digits)| {
+            digits
+                .iter()
+                .map(|s| s.len())
                 .filter(|n| [2, 4, 3, 7].contains(n))
                 .count()
         })
         .sum();
-    println!("n 1,4,7,8 = {}", n);
+    println!("num_easy_identified  is {}", num_easy_identified);
 }
