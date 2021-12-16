188d07a829ed8f791681eed8ab36c1027c7f489e
Michael Daum <michael.daum@epicgames.net>
2021-12-16 07:16:34 -0500

    Another Day 8 WIP

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -5,7 +5,7 @@ use std::io::BufReader;
 
 use itertools::Itertools;
 
-fn read_lines(file_name: &std::string::String) -> Vec<String> {
+fn read_lines(file_name: &str) -> Vec<String> {
     let data_file = File::open(file_name).unwrap();
     let reader = BufReader::new(data_file);
 
@@ -25,9 +25,7 @@ fn count_easily_identified(display_tuples: &Vec<(Vec<String>, Vec<String>)>) -> 
         .sum()
 }
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let data_file_path = &args[1];
+fn get_display_tuples(data_file_path: &str) -> Vec<(Vec<String>, Vec<String>)> {
     let lines = read_lines(data_file_path);
     let tokens: Vec<Vec<Vec<String>>> = lines
         .iter()
@@ -42,40 +40,57 @@ fn main() {
         })
         .collect();
 
-    let display_tuples: Vec<(Vec<String>, Vec<String>)> = tokens
+    tokens
         .into_iter()
-        .map(|v| (v.iter().next().unwrap().clone(), v.iter().nth(1).unwrap().clone()))
-        .collect();
+        .map(|v| {
+            (
+                v.iter().next().unwrap().clone(),
+                v.iter().nth(1).unwrap().clone(),
+            )
+        })
+        .collect()
+}
 
+fn sum_entry(digits: Vec<String>, displays: Vec<String>) {
+    let mut token_map = HashMap::new();
+    let mut char_map = HashMap::new();
+
+    for token in &digits {
+        if let Some(digit) = match token.len() {
+            2 => Some(1),
+            3 => Some(7),
+            4 => Some(4),
+            7 => Some(8),
+            _ => None,
+        } {
+            token_map.insert(digit, token.clone());
+        }
+    }
+
+    for c in token_map.get(&7).unwrap().chars() {
+        if !token_map.get(&1).unwrap().contains(c) {
+            char_map.insert('a', c);
+        }
+    }
+    println!("Here is the token map {:?}", token_map);
+    println!("Here is the char map {:?}", char_map);
+    let tokens_235: Vec<_> = digits.iter().filter(|s| s.len() == 5).collect();
+    println!("235: {:?}", tokens_235);
+    let tokens_069: Vec<_> = digits.iter().filter(|s| s.len() == 6).collect();
+    println!("069: {:?}", tokens_069);
+    println!("");
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let data_file_path = &args[1];
+    let display_tuples = get_display_tuples(&data_file_path);
     println!(
         "num_easy_identified  is {}",
         count_easily_identified(&display_tuples)
     );
 
-    for (unique, _displays) in display_tuples {
-        let mut token_map = HashMap::new();
-		let mut char_map = HashMap::new();
-        for token in &unique {
-            if let Some(digit) = match token.len() {
-                2 => Some(1),
-                3 => Some(7),
-                4 => Some(4),
-                7 => Some(8),
-                _ => None,
-            } {
-                token_map.insert(digit, token.clone());
-            }
-        }
-		for c in token_map.get(&7).unwrap().chars() {
-			if !token_map.get(&1).unwrap().contains(c) {
-				char_map.insert('a', c);
-			}
-		}
-        println!("Here is the token map {:?}", token_map);
-        println!("Here is the char map {:?}", char_map);
-        let tokens_235: Vec<_> = unique.iter().filter(|s| s.len() == 5).collect();
-        println!("235: {:?}", tokens_235);
-        let tokens_069: Vec<_> = unique.iter().filter(|s| s.len() == 6).collect();
-        println!("069: {:?}", tokens_069);
+    for (digits, displays) in display_tuples {
+        sum_entry(digits, displays);
     }
 }
